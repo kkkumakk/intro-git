a207145cdbfcd3fe048b7abbeed9e4ff9dc86479
kkkumakk <10colorharu12262ezweb.ne.jp>
2019-11-14 14:56:00 +0900

    複数ファイルの追加

---
+++ main.js
@@ -0,0 +1,33 @@
+$(function() {
+    $(window).scroll(function() {
+        $('.fadein').each(function() {
+            let elemPos = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200) {
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+});
+
+let scroll = new SmoothScroll('a[href*="#"]', {
+  header: '#header',
+  speed:2000,
+});
+
+$(window).on("scroll", function() {
+  let windowHeight = $(window).height();
+  if ($(this).scrollTop() > windowHeight) {
+    $('.go-top').show();
+  } else {
+    $('.go-top').hide();
+  }
+});
+
+$('.go-top').click(function () {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 1000);
+  return false;
+});
